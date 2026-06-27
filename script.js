67fcb0e53d98eccd5a6e69dce103c713f0a3f48e
Lucille-R <lucillerichard37@yahoo.fr>
2026-06-27 15:54:54 +0200

    Réalise l'étape 4 : invoquer la créature

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+// Etape 3 : Réagir au clic
+
 const formulaire = document.getElementById('invocation');
 
 formulaire.addEventListener('submit', (event)  => {
 	event.preventDefault();
-	console.log("clic !")
+
+	const prenom = document.getElementById('prenom').value;
+	const creature = document.getElementById('creature').value;
+	const image = document.getElementById('imageCreature');
+	const elementMessage = document.getElementById('message');
+
+	let message = "";
+	let imageSource = ""
+
+	if (creature === "le Chat-Octo") {
+		imageSource = "chat.jpg";
+	} else if (creature ==="le Dragon-Debug") {
+		imageSource = "dragon.jpg";
+	} else if (creature === "la Licorne-CSS"){
+		imageSource = "licorne.jpg";
+	} else if (creature ==="le Renard-Refacto") {
+		imageSource = "renard.jpg";
+	}
+
+	message = `${prenom}, ${creature} a entendu ton appel et te suivra tout le Bloc 1 !`;
+	elementMessage.textContent = message;
+	image.src = imageSource;
+	document.getElementById('formulaire').classList.add('cache');
+	document.getElementById('resultat').classList.remove('cache');
 })
+
